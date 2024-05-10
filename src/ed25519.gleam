dae14dc29e1a426f250225e1d49905b31c9638ee
tignear <tignear+m@gmail.com>
2024-05-11 02:09:11 +0900

    update api for future js support

--- src/ed25519.gleam
+++ src/ed25519.gleam
@@ -3,14 +3,16 @@ pub fn verify(
   message: BitArray,
   signature: BitArray,
   public_key: BitArray,
-) -> Bool
+  callback: fn(Bool) -> Nil
+) -> Nil
 
 @external(erlang, "ed25519_shim", "sign")
 pub fn sign(
   message: BitArray,
   public_key: BitArray,
   private_key: BitArray,
-) -> BitArray
-
+  callback: fn(BitArray) -> Nil
+) -> Nil
+// PrivateKey,PublicKey
 @external(erlang, "ed25519_shim", "generate_key")
-pub fn generate_key() -> #(BitArray, BitArray)
+pub fn generate_key(callback: fn(#(BitArray, BitArray))->Nil) -> Nil
--- src/ed25519_shim.erl
+++ src/ed25519_shim.erl
@@ -1,14 +1,14 @@
 -module(ed25519_shim).
--export([verify/3, sign/3, generate_key/0]).
+-export([verify/4, sign/4, generate_key/1]).
 
-verify(Message, Signature, Key) ->
-    public_key:verify(Message, none, Signature, {ed_pub, ed25519, Key}).
+verify(Message, Signature, Key,Callback) ->
+    Callback(public_key:verify(Message, none, Signature, {ed_pub, ed25519, Key})).
 
-sign(Message, PublicKey, PrivateKey) ->
-    public_key:sign(Message, none, {ed_pri, ed25519, PublicKey, PrivateKey}).
+sign(Message, PublicKey, PrivateKey,Callback) ->
+    Callback(public_key:sign(Message, none, {ed_pri, ed25519, PublicKey, PrivateKey})).
 
-generate_key() ->
+generate_key(Callback) ->
     {'ECPrivateKey', 1, PrivateKey, _, PublicKey, asn1_NOVALUE} = public_key:generate_key(
         {namedCurve, ed25519}
     ),
-    {PrivateKey, PublicKey}.
+    Callback({PrivateKey, PublicKey}).
